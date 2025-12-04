009c6f36859af962a7fc4480c5d9c0a96215ff6a
Saul Plascencia <saulplascencia09@gmail.com>
2025-12-04 14:47:00 -0800

    added marquee to main

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Hero3 from "../components/Hero3";
 import Hero2 from "../components/Hero2";
 import Footer from "../components/Footer";
+import MarqueeComponent from "@/components/Marquee";
 
 export default function Home() {
   return (
@@ -12,6 +13,9 @@ export default function Home() {
       <NavBar />
       <Hero />
       <Hero3 />
+      <div className="py-16">
+        <MarqueeComponent />
+      </div>
       <Hero2 />
       <Footer />
     </main>
